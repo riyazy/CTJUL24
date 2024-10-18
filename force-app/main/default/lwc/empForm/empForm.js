6b71c87ca4680c120ad457d185339ee0c7bc7aa8
Mohamed Riyaz Yoonus <info@rangerinc.co>
2024-10-18 10:16:51 +0400

    lastest changes

--- force-app/main/default/lwc/empForm/empForm.js
+++ force-app/main/default/lwc/empForm/empForm.js
@@ -1,17 +1,56 @@
 import { LightningElement } from 'lwc';
 import createEmpApexFn from '@salesforce/apex/EmployeeController.createEmployee';
+import readEmpApexfn from '@salesforce/apex/EmployeeController.readEmployees';
+
+const columns = [
+    { label: 'Id', fieldName: 'Name', type: 'text' },
+    { label: 'Name', fieldName: 'Employee_Name__c' },
+    { label: 'Mobile', fieldName: 'Mobile_Number__c', type: 'phone' }
+];
 
 export default class EmpForm extends LightningElement {
     eName;
     eMobile;
 
+    columns = columns;
+    empList = [];
+
+    constructor() {
+        super();
+    }
+
+    loadFn() {
+        readEmpApexfn({})
+            .then(result => {
+                console.log(result);
+                console.table(result);
+                this.empList = result;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    connectedCallback() {
+        this.loadFn();
+    }
+
+    renderedCallback() {
+
+    }
+
     handleSubmit(e) {
-        console.log('inside handleSubmit');
+        console.log('inside handleSubmit with ' + this.eName);
+
+        this.eName = this.template.querySelector("lightning-input[data-id='nameinput']").value;
+        this.eMobile = this.template.querySelector("lightning-input[data-id='mobileinput']").value;
 
         createEmpApexFn({name: this.eName})
             .then(result => {
                 console.log('inside then');
                 console.log(result);
+                this.template.querySelector("lightning-input[data-id='nameinput']").value = '';
+                this.loadFn();
             })
             .catch(error => {
                 console.log('inside catch');
--- force-app/main/default/triggers/EmployeeAfterTrigger.trigger
+++ force-app/main/default/triggers/EmployeeAfterTrigger.trigger
@@ -8,7 +8,13 @@ trigger EmployeeAfterTrigger on Pharmacy_Employees__c (after insert) {
             String subject = 'Welcome to our Pharmacy, ' + emp.Employee_Name__c;
             String body = 'We hope you enjoy your work!';
             System.debug('Good to send email for ' + emp.Employee_Name__c);
-            EmailManager.sendMail(address, subject, body);
+            try {
+                EmailManager.sendMail(address, subject, body);
+            } catch(Exception e) {
+                emp.addError('Unable to save record' + e.getMessage());
+            }
+        } else {
+            // emp.addError('Please provide email address');
         }
     }
 }
